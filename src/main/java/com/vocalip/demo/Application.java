948a47f0baf83845cffd7ab425b937d9ce2f4541
maksym.hlukhov <holosievo@gmail.com>
2019-06-04 16:16:07 +0200

    Initial commit

---
+++ src/main/java/com/vocalip/demo/Application.java
@@ -0,0 +1,51 @@
+package com.vocalip.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+
+@Slf4j
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+    @Bean
+    JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                             DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        // This provides all boot's default to this factory, including the message converter
+        configurer.configure(factory, connectionFactory);
+        // You could still override some of Boot's default if necessary.
+        return factory;
+    }
+
+    @Bean
+    MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+        log.info("Sending an email message.");
+        jmsTemplate.convertAndSend("mailbox",
+                new Email("holosievo@gmail.com", "Hello!"));
+    }
+}
---
+++ src/main/java/com/vocalip/demo/Receiver.java
@@ -0,0 +1,15 @@
+package com.vocalip.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        log.info("Received <{}>", email);
+    }
+}
